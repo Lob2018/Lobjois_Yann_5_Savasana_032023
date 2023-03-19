5af1377abcbc2e8d31319eac1e3db1021e3ce407
Lob <37583137+Lob2018@users.noreply.github.com>
2023-03-19 19:58:24 +0100

    Test : Angular unit and integration tests Completed : 	app 	app/features/auth/components/login

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,29 +1,118 @@
 import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
+import { of } from 'rxjs';
 
 import { AppComponent } from './app.component';
 
+describe('AppComponent - Given I am on the root page', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let toolbar: DebugElement[];
 
-describe('AppComponent', () => {
+  let router: Router;
+
+  // Mock called components
+  @Component({
+    selector: 'app-login',
+    template: '',
+  })
+  class MockLoginComponent {}
+  @Component({
+    selector: 'app-register',
+    template: '',
+  })
+  class MockRegisterComponent {}
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      declarations: [AppComponent],
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'login', component: MockLoginComponent },
+          { path: 'register', component: MockRegisterComponent },
+        ]),
         HttpClientModule,
-        MatToolbarModule
-      ],
-      declarations: [
-        AppComponent
+        MatToolbarModule,
+        ReactiveFormsModule,
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    fixture.detectChanges();
+    toolbar = fixture.debugElement.queryAll(By.css('span'));
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('Then AppComponent should be created', () => {
+    expect(component).toBeTruthy();
+  });
+  it('Then by default we are not connected', () => {
+    expect(component.$isLogged()).toBeTruthy();
+  });
+  it('Then the application name, Login and Register should be shown', fakeAsync(async () => {
+    fixture.detectChanges();
+    expect(toolbar[0].nativeElement.textContent).toContain('Yoga app');
+    expect(toolbar[1].nativeElement.textContent).toContain('Login');
+    expect(toolbar[2].nativeElement.textContent).toContain('Register');
+  }));
+  describe('When I click on Login button', () => {
+    it('Then I am redirected on the login page', fakeAsync(async () => {
+      fixture.detectChanges();
+      toolbar[1].nativeElement.click();
+      tick(500);
+      fixture.detectChanges();
+      expect(router.url).toBe('/login');
+    }));
+  });
+  describe('When I click on Register button', () => {
+    it('Then I am redirected on the register page', fakeAsync(async () => {
+      fixture.detectChanges();
+      toolbar[2].nativeElement.click();
+      tick(500);
+      fixture.detectChanges();
+      expect(router.url).toBe('/register');
+    }));
+  });
+  describe('When I am logged', () => {
+    it('Then the application name, Sessions Account and Logout should be shown', fakeAsync(async () => {
+      jest.spyOn(component, '$isLogged').mockReturnValue(of(true));
+      tick(500);
+      fixture.detectChanges();
+      let toolbar = fixture.debugElement.queryAll(By.css('span'));
+
+      expect(component.$isLogged()).toBeTruthy();
+      expect(toolbar[0].nativeElement.textContent).toContain('Yoga app');
+      expect(toolbar[1].nativeElement.textContent).toContain('Sessions');
+      expect(toolbar[2].nativeElement.textContent).toContain('Account');
+      expect(toolbar[3].nativeElement.textContent).toContain('Logout');
+    }));
+  });
+  describe('When I am logged and I click on Logout button', () => {
+    it('Then I am disconnected and redirected to the landing page', fakeAsync(async () => {
+      jest.spyOn(component, '$isLogged').mockReturnValue(of(true));
+      const logoutSpy = jest.spyOn(component, 'logout');
+      tick(500);
+      fixture.detectChanges();
+      let toolbar = fixture.debugElement.queryAll(By.css('span'));
+      expect(component.$isLogged()).toBeTruthy();
+
+      toolbar[3].nativeElement.click();
+      tick(500);
+      fixture.detectChanges();
+      expect(logoutSpy).toHaveBeenCalled();
+      expect(router.url).toBe('/');
+    }));
   });
 });
--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,42 +1,178 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
+import { of, throwError } from 'rxjs';
+import { SessionsModule } from 'src/app/features/sessions/sessions.module';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
 import { SessionService } from 'src/app/services/session.service';
+import { LoginRequest } from '../../interfaces/loginRequest.interface';
+import { AuthService } from '../../services/auth.service';
 
 import { LoginComponent } from './login.component';
 
-describe('LoginComponent', () => {
+describe('LoginComponent - Given I am on the login page', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  let serviceAuthLogin: AuthService;
+  let serviceSession: SessionService;
+
+  let router: Router;
+
+  const loginRequest: LoginRequest = {
+    email: 'yoga@studio.com',
+    password: 'test!1234',
+  };
+  const mockedLoginResponse200: SessionInformation = {
+    token:
+      'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5b2dhQHN0dWRpby5jb20iLCJpYXQiOjE2Nzg5NjQwNjQsImV4cCI6MTY3OTA1MDQ2NH0.ZHTp0XHK6IaWLzTKLHwyJXeDOAsyu9RPLgUBUjaniDulLLttRlWP-nVfZCpjIqn6Zb-xAumtPPLVxR9L2L17gA',
+    type: 'Bearer',
+    id: 1,
+    username: 'yoga@studio.com',
+    firstName: 'Admin',
+    lastName: 'Admin',
+    admin: true,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [SessionService, AuthService],
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'sessions', component: SessionsModule },
+        ]),
         BrowserAnimationsModule,
         HttpClientModule,
         MatCardModule,
         MatIconModule,
         MatFormFieldModule,
         MatInputModule,
-        ReactiveFormsModule]
-    })
-      .compileComponents();
+        ReactiveFormsModule,
+      ],
+    }).compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
+    serviceAuthLogin = TestBed.inject(AuthService);
+    serviceSession = TestBed.inject(SessionService);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Then LoginComponent should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Then the submit button should be disabled and the form invalid', fakeAsync(async () => {
+    fixture.detectChanges();
+    const submitButton = fixture.debugElement.query(
+      By.css('button[type="submit"]')
+    );
+    expect(submitButton.nativeElement.disabled).toBeTruthy();
+    expect(component.form.valid).toBeFalsy();
+  }));
+
+  describe('When I click on the password eye icon', () => {
+    it('Then password type toggle from password to text', fakeAsync(async () => {
+      fixture.detectChanges();
+      const toggleButton = fixture.debugElement.query(
+        By.css('button[aria-label="Hide password"]')
+      );
+      const password = fixture.debugElement.nativeElement.querySelector(
+        'input[formcontrolname="password"]'
+      );
+      expect(password.type).toBe('password');
+      toggleButton.triggerEventHandler('click', null);
+      tick(500);
+      fixture.detectChanges();
+      expect(password.type).toBe('text');
+      toggleButton.triggerEventHandler('click', null);
+      tick(500);
+      fixture.detectChanges();
+      expect(password.type).toBe('password');
+    }));
+  });
+  describe('When I fill the login form with an invalid email', () => {
+    it('Then the form is invalid', fakeAsync(async () => {
+      fixture.detectChanges();
+      const email = component.form.controls['email'];
+      email.setValue('yogastudio.com');
+      const password = component.form.controls['password'];
+      password.setValue(loginRequest.password);
+      tick(1000);
+      fixture.detectChanges();
+      expect(component.form.valid).toBeFalsy();
+    }));
+  });
+  describe('When I fill the login form with no password', () => {
+    it('Then the form is invalid', fakeAsync(async () => {
+      fixture.detectChanges();
+      const email = component.form.controls['email'];
+      email.setValue(loginRequest.email);
+      const password = component.form.controls['password'];
+      password.setValue('');
+      tick(1000);
+      fixture.detectChanges();
+      expect(component.form.valid).toBeFalsy();
+    }));
+  });
+  describe('When I send the login form with invalid credentials', () => {
+    it('Then we should get a message, and the router is not called', async () => {
+      fixture.detectChanges();
+      const navigateSpy = jest.spyOn(router, 'navigate');
+      jest
+        .spyOn(serviceAuthLogin, 'login')
+        .mockReturnValue(throwError(() => new Error('')));
+
+      serviceAuthLogin.login({ email: '', password: '' }).subscribe();
+
+      // Arrange
+      const el = fixture.debugElement.query(By.css('.login-form'));
+      el.triggerEventHandler('submit', {});
+
+      // Assert
+      fixture.detectChanges();
+
+      const msg = fixture.debugElement.query(By.css('.error')).nativeElement;
+      expect(msg).toBeTruthy();
+      expect(navigateSpy).not.toBeCalled();
+    });
+  });
+  describe('When I send the login form with valid credentials', () => {
+    it('Then submit method is called and we are redirected to /sessions', async () => {
+      fixture.detectChanges();
+      const navigateSpy = jest.spyOn(router, 'navigate');
+      jest.spyOn(component, 'submit');
+      jest.spyOn(serviceSession, 'logIn');
+      jest
+        .spyOn(serviceAuthLogin, 'login')
+        .mockReturnValue(of(mockedLoginResponse200));
+
+      serviceSession.logIn(mockedLoginResponse200);
+      serviceAuthLogin.login(loginRequest).subscribe();
+
+      // Arrange
+      const el = fixture.debugElement.query(By.css('.login-form'));
+      el.triggerEventHandler('submit', {});
+      fixture.detectChanges();
+
+      // Assert
+      expect(component.submit).toHaveBeenCalled();
+      expect(navigateSpy).toHaveBeenCalledWith(['/sessions']);
+    });
   });
 });
