275a7f4c8f2a05be6c6238d53b7aaf31adfb0ca4
Lob <37583137+Lob2018@users.noreply.github.com>
2023-03-28 23:07:41 +0200

    Refactor : tests Back-End 	SecurityServices 		UserDetailsImpl 		UserDetailsServiceImpl

--- back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsImplTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsImplTest.java
@@ -4,14 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
-
 import java.util.Collection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.GrantedAuthority;
--- back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsServiceTest.java
@@ -1,33 +1,15 @@
 package com.openclassrooms.starterjwt.security.services;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.Optional;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -55,7 +37,7 @@ public class UserDetailsServiceTest {
 	private final User user = new User(id, mail, lastName, firstName, password, false, localDateTime, localDateTime);
 
 	@Test
-	public void loadUserByUsername_UserFound_ReturnsUserDetails() {
+	public void loadUserByUsername_success() {
 		// GIVEN
 		when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
 		// WHEN
@@ -68,7 +50,7 @@ public class UserDetailsServiceTest {
 	}
 
 	@Test
-	public void loadUserByUsername_UserNotFound_ThrowsException() {
+	public void loadUserByUsernameThrowsUserNotFound_fail() {
 		// GIVEN
 		when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.empty());
 		try {
